84fb9df4dd455dbfee8638a31d3e4eb73406edb1
Artem Dirksen <dirksen.tema@gmail.com>
2024-12-12 14:18:34 +0200

    added form adn social links

---
+++ assets/js/pop.js
@@ -0,0 +1,8 @@
+$('.pop_btn').on('click', function () {
+    $('#pop').toggleClass('active');
+    $('body').toggleClass('overflow');
+})
+$('.pop_close').on('click', function () {
+    $('#pop').toggleClass('active');
+    $('body').toggleClass('overflow');
+})
